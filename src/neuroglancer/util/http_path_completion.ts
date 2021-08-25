Andrew-Keenlyside/neuroglancer

commit daab0a79b2f9cc89d6701fdc5de3fc4fcaf302a1

feat(special_protocol): support unauthenticated s3 via s3://

src/neuroglancer/util/http_path_completion.ts
@@ -18,9 +18,9 @@ import {CredentialsManager} from 'neuroglancer/credentials_provider';
 import {CancellationToken} from 'neuroglancer/util/cancellation';
 import {BasicCompletionResult, Completion, CompletionWithDescription, getPrefixMatchesWithDescriptions} from 'neuroglancer/util/completion';
 import {getGcsPathCompletions} from 'neuroglancer/util/gcs_bucket_listing';
-import {parseUrl} from 'neuroglancer/util/http_request';
-import {cancellableFetchOk} from 'neuroglancer/util/http_request';
-import {getS3PathCompletions} from 'neuroglancer/util/s3_bucket_listing';
+import {cancellableFetchOk, parseUrl} from 'neuroglancer/util/http_request';
+import {getS3PathCompletions} from 'neuroglancer/util/s3';
+import {getS3CompatiblePathCompletions} from 'neuroglancer/util/s3_bucket_listing';
 import {parseSpecialUrl} from 'neuroglancer/util/special_protocol_request';
 
 /**
@@ -70,10 +70,23 @@ export async function getHtmlPathCompletions(
 const specialProtocolEmptyCompletions: CompletionWithDescription[] = [
   {value: 'gs://', description: 'Google Cloud Storage (JSON API)'},
   {value: 'gs+xml://', description: 'Google Cloud Storage (XML API)'},
-  {value: 'gs+ngauth+http://', description: 'Google Cloud Storage (JSON API) authenticated via ngauth'},
-  {value: 'gs+ngauth+https://', description: 'Google Cloud Storage (JSON API) authenticated via ngauth'},
-  {value: 'gs+xml+ngauth+http://', description: 'Google Cloud Storage (XML API) authenticated via ngauth'},
-  {value: 'gs+xml+ngauth+https://', description: 'Google Cloud Storage (XML API) authenticated via ngauth'},
+  {
+    value: 'gs+ngauth+http://',
+    description: 'Google Cloud Storage (JSON API) authenticated via ngauth'
+  },
+  {
+    value: 'gs+ngauth+https://',
+    description: 'Google Cloud Storage (JSON API) authenticated via ngauth'
+  },
+  {
+    value: 'gs+xml+ngauth+http://',
+    description: 'Google Cloud Storage (XML API) authenticated via ngauth'
+  },
+  {
+    value: 'gs+xml+ngauth+https://',
+    description: 'Google Cloud Storage (XML API) authenticated via ngauth'
+  },
+  {value: 's3://', description: 'Amazon Simple Storage Service (S3)'},
   {value: 'https://'},
   {value: 'http://'},
 ];
@@ -100,17 +113,19 @@ export async function completeHttpPath(
   const {protocol, host, path} = result;
   const completions = await (async () => {
     if (protocol === 'gs+xml' && path.length > 0) {
-      return await getS3PathCompletions(
+      return await getS3CompatiblePathCompletions(
           credentialsProvider, `${protocol}://${host}`, `https://storage.googleapis.com/${host}`,
           path, cancellationToken);
     } else if (protocol === 'gs' && path.length > 0) {
       return await getGcsPathCompletions(
           credentialsProvider, `${protocol}://${host}`, host, path, cancellationToken);
+    } else if (protocol === 's3' && path.length > 0) {
+      return await getS3PathCompletions(host, path, cancellationToken);
     }
     const s3Match = parsedUrl.match(
-        /^((?:http|https):\/\/(?:storage\.googleapis\.com\/[^\/]+|[^\/]+\.storage\.googleapis\.com))(\/.*)$/);
+        /^((?:http|https):\/\/(?:storage\.googleapis\.com\/[^\/]+|[^\/]+\.storage\.googleapis\.com|[^\/]+|[^\/]+\.s3(?:[^./]+)?\.amazonaws.com))(\/.*)$/);
     if (s3Match !== null) {
-      return await getS3PathCompletions(
+      return await getS3CompatiblePathCompletions(
           credentialsProvider, s3Match[1], s3Match[1], s3Match[2], cancellationToken);
     }
     if ((protocol === 'http' || protocol === 'https') && path.length > 0) {

src/neuroglancer/util/s3.ts
@@ -1,0 +1,75 @@
+/**
+ * @license
+ * Copyright 2021 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {CachingMapBasedCredentialsManager, CredentialsProvider, makeCredentialsGetter} from 'neuroglancer/credentials_provider';
+import {CancellationToken, uncancelableToken} from 'neuroglancer/util/cancellation';
+import {cancellableFetchOk, ResponseTransform} from 'neuroglancer/util/http_request';
+import {getS3CompatiblePathCompletions} from 'neuroglancer/util/s3_bucket_listing';
+
+// Support for s3:// special protocol.
+
+interface S3BucketCredentials {
+  region: string;
+}
+
+class S3RegionProvider extends CredentialsProvider<S3BucketCredentials> {
+  constructor(public bucket: string) {
+    super();
+  }
+  get = makeCredentialsGetter(async () => {
+    const {bucket} = this;
+    const response =
+        await cancellableFetchOk(`https://s3.amazonaws.com/${bucket}?location`, {}, x => x.text());
+    const doc = new DOMParser().parseFromString(response, 'application/xml');
+    const locationElement = doc.querySelector('LocationConstraint');
+    if (locationElement === null) {
+      throw new Error(`Unable to determine location of S3 bucket: ${bucket}`);
+    }
+    const location = locationElement.textContent?.trim() || 'us-east-1';
+    return {region: location};
+  });
+}
+
+let s3RegionCache: CachingMapBasedCredentialsManager|undefined;
+
+export function getS3RegionCredentials(bucket: string) {
+  if (s3RegionCache === undefined) {
+    s3RegionCache = new CachingMapBasedCredentialsManager();
+    s3RegionCache.register('s3', bucket => new S3RegionProvider(bucket));
+  }
+  return s3RegionCache.getCredentialsProvider('s3', bucket);
+}
+
+export async function cancellableFetchS3Ok<T>(
+    credentialsProvider: CredentialsProvider<S3BucketCredentials>, bucket: string, path: string,
+    requestInit: RequestInit, transformResponse: ResponseTransform<T>,
+    cancellationToken: CancellationToken = uncancelableToken) {
+  const credentials = await credentialsProvider.get();
+  const {region} = credentials.credentials;
+  return await cancellableFetchOk(
+      `https://${bucket}.s3.${region}.amazonaws.com${path}`, requestInit, transformResponse,
+      cancellationToken);
+}
+
+export async function getS3PathCompletions(
+    bucket: string, path: string, cancellationToken: CancellationToken) {
+  const credentialsProvider = getS3RegionCredentials(bucket);
+  const credentials = await credentialsProvider.get();
+  const {region} = credentials.credentials as S3BucketCredentials;
+  return await getS3CompatiblePathCompletions(
+      undefined, `s3://${bucket}`, `https://${bucket}.s3.${region}.amazonaws.com`, path,
+      cancellationToken);
+}

src/neuroglancer/util/s3_bucket_listing.ts
@@ -49,7 +49,7 @@ export async function getS3BucketListing(
   return results;
 }
 
-export async function getS3PathCompletions(
+export async function getS3CompatiblePathCompletions(
     credentialsProvider: SpecialProtocolCredentialsProvider, enteredBucketUrl: string,
     bucketUrl: string, path: string,
     cancellationToken: CancellationToken): Promise<BasicCompletionResult> {

src/neuroglancer/util/special_protocol_request.ts
@@ -15,18 +15,18 @@
  */
 
 import {CredentialsManager, MaybeOptionalCredentialsProvider} from 'neuroglancer/credentials_provider';
-import {fetchWithOAuth2Credentials, OAuth2Credentials} from 'neuroglancer/credentials_provider/oauth2';
+import {fetchWithOAuth2Credentials} from 'neuroglancer/credentials_provider/oauth2';
 import {CancellationToken, uncancelableToken} from 'neuroglancer/util/cancellation';
 import {parseUrl, ResponseTransform} from 'neuroglancer/util/http_request';
+import {cancellableFetchS3Ok, getS3RegionCredentials} from 'neuroglancer/util/s3';
 
-export type SpecialProtocolCredentials = OAuth2Credentials|undefined;
+export type SpecialProtocolCredentials = any;
 export type SpecialProtocolCredentialsProvider =
-  MaybeOptionalCredentialsProvider<SpecialProtocolCredentials>;
+    MaybeOptionalCredentialsProvider<SpecialProtocolCredentials>;
 
 function getMiddleAuthCredentialsProvider(
     credentialsManager: CredentialsManager, url: string): SpecialProtocolCredentialsProvider {
-  return credentialsManager.getCredentialsProvider(
-    'middleauthapp', new URL(url).origin);
+  return credentialsManager.getCredentialsProvider('middleauthapp', new URL(url).origin);
 }
 
 function getNgauthCredentialsProvider(
@@ -55,22 +55,26 @@ export function parseSpecialUrl(url: string, credentialsManager: CredentialsMana
       };
     case 'gs+ngauth+http':
       return {
-        credentialsProvider: getNgauthCredentialsProvider(credentialsManager, `http://${u.host}`, u.path),
+        credentialsProvider:
+            getNgauthCredentialsProvider(credentialsManager, `http://${u.host}`, u.path),
         url: 'gs:/' + u.path,
       };
     case 'gs+ngauth+https':
       return {
-        credentialsProvider: getNgauthCredentialsProvider(credentialsManager, `https://${u.host}`, u.path),
+        credentialsProvider:
+            getNgauthCredentialsProvider(credentialsManager, `https://${u.host}`, u.path),
         url: 'gs:/' + u.path,
       };
     case 'gs+xml+ngauth+http':
       return {
-        credentialsProvider: getNgauthCredentialsProvider(credentialsManager, `http://${u.host}`, u.path),
+        credentialsProvider:
+            getNgauthCredentialsProvider(credentialsManager, `http://${u.host}`, u.path),
         url: 'gs+xml:/' + u.path,
       };
     case 'gs+xml+ngauth+https':
       return {
-        credentialsProvider: getNgauthCredentialsProvider(credentialsManager, `https://${u.host}`, u.path),
+        credentialsProvider:
+            getNgauthCredentialsProvider(credentialsManager, `https://${u.host}`, u.path),
         url: 'gs+xml:/' + u.path,
       };
     case 'middleauth+https':
@@ -78,6 +82,11 @@ export function parseSpecialUrl(url: string, credentialsManager: CredentialsMana
       return {
         credentialsProvider: getMiddleAuthCredentialsProvider(credentialsManager, url),
         url: url,
+      };
+    case 's3':
+      return {
+        credentialsProvider: getS3RegionCredentials(u.host),
+        url,
       };
     default:
       return {
@@ -116,6 +125,9 @@ export async function cancellableFetchSpecialOk<T>(
       return fetchWithOAuth2Credentials(
           credentialsProvider, `https://storage.googleapis.com/${u.host}${u.path}`, init,
           transformResponse, cancellationToken);
+    case 's3':
+      return cancellableFetchS3Ok(
+          credentialsProvider!, u.host, u.path, init, transformResponse, cancellationToken);
     default:
       return fetchWithOAuth2Credentials(
           credentialsProvider, url, init, transformResponse, cancellationToken);

commit 6aad4cc38ffbf19e318781ee74ce1f8d8a0786a4

fix(http_path_completion): fix HTML-based path completion
This fixes the regression introduced by daab0a79b2f9cc89d6701fdc5de3fc4fcaf302a1.

src/neuroglancer/util/http_path_completion.ts
@@ -123,7 +123,7 @@ export async function completeHttpPath(
       return await getS3PathCompletions(host, path, cancellationToken);
     }
     const s3Match = parsedUrl.match(
-        /^((?:http|https):\/\/(?:storage\.googleapis\.com\/[^\/]+|[^\/]+\.storage\.googleapis\.com|[^\/]+|[^\/]+\.s3(?:[^./]+)?\.amazonaws.com))(\/.*)$/);
+        /^((?:http|https):\/\/(?:storage\.googleapis\.com\/[^\/]+|[^\/]+\.storage\.googleapis\.com|[^\/]+\.s3(?:[^./]+)?\.amazonaws.com))(\/.*)$/);
     if (s3Match !== null) {
       return await getS3CompatiblePathCompletions(
           credentialsProvider, s3Match[1], s3Match[1], s3Match[2], cancellationToken);